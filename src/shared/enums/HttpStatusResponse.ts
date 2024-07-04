80e8488b8a35bc43161d460de412f79ab6c3066b
EmanuelErnesto <emanuel.earego@gmail.com>
2024-07-04 00:52:15 -0300

    feat: add enum HttpStatusResponse

---
+++ src/shared/enums/HttpStatusResponse.ts
@@ -0,0 +1,7 @@
+export enum HttpStatusResponse {
+  BAD_REQUEST = 'Bad Request',
+  FORBIDDEN = 'Forbidden',
+  UNAUTHORIZED = 'Unauthorized',
+  NOT_FOUND = 'Not found',
+  INTERNAL_SERVER = 'Internal Server Error',
+}
