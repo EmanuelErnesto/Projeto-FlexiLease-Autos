d0c2d51fb8388176f3715f7e19ada3bc3cbca678
EmanuelErnesto <emanuel.earego@gmail.com>
2024-07-04 00:59:50 -0300

    feat: add typeorm datasource

---
+++ src/shared/infra/typeorm/index.ts
@@ -0,0 +1,12 @@
+import { DataSource } from 'typeorm';
+import { User } from '@modules/users/infra/typeorm/entities/User';
+import 'dotenv/config';
+
+export const dataSource = new DataSource({
+  type: 'mongodb',
+  database: process.env.DATABASE,
+  synchronize: false,
+  host: 'localhost',
+  port: 27017,
+  entities: [User],
+});
